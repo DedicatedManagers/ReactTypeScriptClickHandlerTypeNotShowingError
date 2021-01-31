53e51d0fd796a73c7146e787a5846ca48077a2f7
TerryCaliendo <Terry@DedicatedManagers.com>
2021-01-30 21:06:47 -0800

    why doesn't this have an error on line 8

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+
+type clickHandler = (num:number)=>void
+
+function MyComponent(){
+  const [count, setCount] = useState<number>(0);
+  const handleClick:clickHandler = () => {setCount(count+1)} // Why isn't arrow function showing a compile error in the params?
+  return (
+    <div>
+      my component {count}<br />
+      <button onClick={()=>handleClick(34)}>Increment</button><br />
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MyComponent></MyComponent>
     </div>
   );
 }
